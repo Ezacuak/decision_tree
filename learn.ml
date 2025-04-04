52581ff9033e0b47104b0de5aec83ba0f5d7922f
antonin <antonin.plard@protonmail.com>
2025-04-04 09:21:09 +0200

    Initial Commit

---
+++ learn.ml
@@ -0,0 +1,11 @@
+(* learn.ml *)
+(*
+
+Cette phase consiste à fournir à l’ordinateur des exemples de documents avec leur catégorie,
+« intéressant » ou « pas intéressant ». En analysant le contenu de chaque document,
+l’ordinateur construit une fonction de décision. C’est la phase d’apprentissage ; l’ensemble
+des documents utilisé pour cette phase est appelé Doc_appr (documents d’apprentissage);
+
+*)
+
+let () = print_endline "Phase d'apprentissage"
