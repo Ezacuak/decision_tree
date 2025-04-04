ee4747891280a78b6e0d7bc8b6b2ba59fbcb0e75
antonin <antonin.plard@protonmail.com>
2025-04-04 09:29:32 +0200

    [add] test zone

--- main.ml
+++ main.ml
@@ -29,4 +29,18 @@ type decision_tree =
     | Leaf of bool
     | Node of string * decision_tree * decision_tree
 
+
 let () = print_endline "Hello World !"
+
+(**************************)
+(**                      **)
+(**     Zone de test     **)
+(**                      **)
+(**************************)
+
+let sport_tree = Node("Plonger",
+    Node("Tournoi", Leaf true,
+        Node("Gagner", Leaf true, 
+            Node("Final", Leaf true, Leaf false))),
+    Leaf false)
+
