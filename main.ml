3e77b966a289b8abf793c59822055451416e28c2
antonin <antonin.plard@protonmail.com>
2025-04-04 10:20:38 +0200

    [add] definition des type pour le format des documents/données

--- main.ml
+++ main.ml
@@ -31,10 +31,25 @@ type decision_tree =
 
 (* NOTE: Format des donnée sous la forme suivante: *)
 (*
-
+    Document -> data list
+    data -> (word list, decision)
+    word -> srting
+    decision -> bool
 *)
 
-let () = print_endline "Hello World !"
+type word = string
+type decision = bool
+type data = word list * decision
+type document = data list
+
+(*************************************)
+(**                                 **)
+(**                                 **)
+(**     Construction d'un arbre     **)
+(**     A partire d'un document     **)
+(**                                 **)
+(**                                 **)
+(*************************************)
 
 
 
@@ -43,6 +58,14 @@ let () = print_endline "Hello World !"
 (**     Zone de test     **)
 (**                      **)
 (**************************)
+
+let sport_doc = [
+    (["tournoi"; "Irlande";"retrouver";"goût"; "victoire"], true);
+    (["ligue"; "champion";"Paris";"demi-finale"], true);
+    (["AMD"; "gagner";"course";"processeur"], false);
+    (["JO"; "Paris";"Romain Ntamak";"porter"; "flamme"; "olympique"; "Occitanie"], true);
+    (["JO"; "Paris";"menace";"grève"; "SNCF"], false)
+]
 
 let sport_tree = Node("Plonger",
     Node("Tournoi", Leaf true,
